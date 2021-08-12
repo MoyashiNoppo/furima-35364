36194e99071b253eaea138cbd8f504f4f7717eef
MoyashiNoppo <moyashin18@gmail.com>
2021-08-12 09:03:04 +0900

    商品データ登録の実装

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,21 @@
+class ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, 
+      :shipping_charge_id, :ship_from_area_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+  
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :days_to_ship
+  belongs_to :ship_from_area
+  belongs_to :shipping_charge
+  
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+
+    with_options numericality: { other_than: 1 , message: "can't be blank"} do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_charge_id
+      validates :ship_from_area_id
+      validates :days_to_ship_id
+    end
+  end
 end
