991a7d485fdeac6957a697b3cd24787ed06fd0a3
MoyashiNoppo <moyashin18@gmail.com>
2021-08-16 21:12:30 +0900

    商品一覧機能の実装

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,4 +1,5 @@
 class FurimasController < ApplicationController
   def index 
+    @items = Item.order("created_at DESC")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'furimas#index'
-
+  
+  resources :frimas, only: [:index]
   resources :items, only: [:new, :create]
 end
