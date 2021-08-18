11aa6ba438be4d5a8e6a3b76495718b1b3f97a09
MoyashiNoppo <moyashin18@gmail.com>
2021-08-18 20:09:20 +0900

    編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
- 
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -24,12 +24,28 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if @item.user != current_user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to item_path
+      else
+        render :edit
+      end
   end
 
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, 
       :shipping_charge_id, :ship_from_area_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+
+  def ensure_user
+    @items = current_user
   end
   
 end
