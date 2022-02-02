ccebe0e036030b5918c66a05353d99483d196298
MoyashiNoppo <moyashin18@gmail.com>
2022-02-03 08:09:25 +0900

    エラーメッセージの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
       validates :name, length: {maximum: 50, allow_blank:true}
       validates :description, length: {maximum:1000, allow_blank:true}
       validates :price, numericality: {allow_blank: true, only_integer: true, greater_than_or_equal_to:300, less_than_or_equal_to:9999999}
-    with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    with_options numericality: { other_than: 1 , message: "を入力してください"} do
       validates :category_id
       validates :condition_id
       validates :shipping_charge_id
